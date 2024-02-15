facb4e693aaa3522efd907ca6a5c565d9a89b470
dinisov <dgokaydin@gmail.com>
2024-02-15 15:47:16 +1000

    Matt fixed reduce_files

--- Utilities/reduce_files.m
+++ Utilities/reduce_files.m
@@ -4,53 +4,64 @@ close all; clear;
 
 rdmDirectory = '\\uq.edu.au\uq-inst-gateway1\RFDG2021-Q4413\2P_Data\Gcamp7s_CC\';
 
-scratchDirectory = '../../2P Data';
-
-flies = readtable('../../2P Record/2P_record');
+blocks = readtable('../../2P Record/2P_record');
 
 %get rid of excluded flies
-flies = flies(~logical(flies.Exclude),:);
+blocks = blocks(~logical(blocks.Exclude),:);
 
 % the numbers here should be the original size divided by some power of 2
 finalSize = [128 128];
 
 %%
 
+chosenFlies = [106];
+chosenBlocks = []; % leave empty if aligning all blocks for one fly
+
+flagParamSaveList = who;
+flagParamSaveList = [flagParamSaveList;'flagParamSaveList';'fly'];
+
 % this level is the list of blocks
-for fly =[136]%height(flies)
+%for fly =[185:187]%height(flies)
+for fly = chosenFlies
+    clearvars('-except', flagParamSaveList{:});
     
-    currentFly = flies(fly,:);
+    % the blocks corresponding to this fly
+    thisFlyBlocks = blocks(blocks.Fly == fly,:);
+
+    if ~isempty(chosenBlocks)
+        thisFlyBlocks = thisFlyBlocks(ismember(thisFlyBlocks.Block,chosenBlocks),:);
+    end
+
+    nBlocks = height(thisFlyBlocks);
     
-    currentDate = char(datetime(currentFly.Date,'Format','dMMMyy'));
-    currentFlyDirectory = ['fly' num2str(currentFly.FlyOnDay) '_exp' num2str(currentFly.Block) '_' currentDate];
+    %currentDate = char(datetime(currentFly.Date,'Format','dMMMyy'));
+    currentDate = char(datetime(thisFlyBlocks.Date(1),'Format','dMMMyy'));
     
-    currentRDMDirectory = fullfile(rdmDirectory,currentDate,currentFlyDirectory);
-    currentScratchDirectory = fullfile(scratchDirectory,currentDate,currentFlyDirectory);
+    %currentFlyDirectory = ['fly' num2str(currentFly.FlyOnDay) '_exp' num2str(currentFly.Block) '_' currentDate];
+    for b = 1:nBlocks
+        currentBlock = thisFlyBlocks(b,:);
+        flyID = ['fly' num2str(currentBlock.FlyOnDay) '_exp' num2str(currentBlock.Block) '_' currentDate]; %Borrowed from pre_process
 
-    disp(currentFlyDirectory);
+        currentRDMDirectory = fullfile(rdmDirectory,currentDate,flyID);
 
-    loadReduceSave(currentRDMDirectory,currentScratchDirectory, 'green_channel.raw', currentFly, finalSize);
-%         loadReduceSave([currentDirectory '\red_channel.raw'], currentFly, finalSize);
+        %disp(currentFlyDirectory);
+        disp(['Fly: ',flyID]);
+        
+        codeStartTime = posixtime(datetime('now'));
+        loadReduceSave(currentRDMDirectory, 'green_channel.raw', currentBlock, finalSize); %currentBlock corresponds to currentFly
 
+        codeEndTime = posixtime(datetime('now'));
+        MET = codeEndTime - codeStartTime;
+        disp(['-- Total time to process: ',num2str(MET),'s --']) 
+    end
 end
 
-function loadReduceSave(RDMDirectory, scratchDirectory, file, fly, finalSize)
+function loadReduceSave(RDMDirectory, file, fly, finalSize)
 
     fileRDM = fullfile(RDMDirectory, file);
-%     fileScratch = fullfile(scratchDirectory, file);
     
     imageSize = [fly.pixelX fly.pixelY];
     nFrames = fly.realFrames;
-    
-    if ~exist(scratchDirectory,'dir')
-       mkdir(scratchDirectory); 
-    end
-    
-    % move file to scratch before loading
-%     disp('Copying file from RDM');
-%     tic
-%     copyfile(fileRDM,fileScratch);
-%     toc
     
     tic
     disp('Loading data');
@@ -60,12 +71,9 @@ function loadReduceSave(RDMDirectory, scratchDirectory, file, fly, finalSize)
     fclose(fid);
     toc
     
-    disp('Rearranging');
-    tic
     %rearrange
     data = permute(reshape(data, [imageSize nFrames]),[2 1 3]);
-    toc
-    
+
     rData = zeros([finalSize nFrames]);
     
     tic
@@ -74,30 +82,15 @@ function loadReduceSave(RDMDirectory, scratchDirectory, file, fly, finalSize)
     rData = imresize3(data,size(rData),'box');
     toc
     
-%     reducedFileScratch = fullfile(scratchDirectory, ['green_channel_' num2str(finalSize(1)) 'x' num2str(finalSize(2)) '.mat']);
     reducedFileRDM = fullfile(RDMDirectory, ['green_channel_' num2str(finalSize(1)) 'x' num2str(finalSize(2)) '.mat']);
     
     tic
     disp('Saving');
     % save green channel; do not compress we care about speed not size
-%     save([path(1:end-4) '_' num2str(finalSize(1)) 'x' num2str(finalSize(2)) '.mat'], 'rData','-v7.3','-nocompression');
     save(reducedFileRDM, 'rData','-v7.3','-nocompression');
     toc
     
-    %move file back to RDM
-%     disp('Moving back to RDM');
-%     tic
-%     copyfile(reducedFileScratch,reducedFileRDM);
-%     toc
-    
-    %delete file in scratch
-%     disp('Deleting files in scratch');
-%     tic
-%     delete(fileScratch);
-%     delete(reducedFileScratch);
-%     toc
-    
-    % free up the memory ?
+    % free up the memory 
     clear;
     
 end
