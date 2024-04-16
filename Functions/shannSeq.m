b5e82f92cfa78a28a6bd4d5d1255a46b30196b39
MNVDP <m.vandepoll@uq.edu.au>
2024-04-16 17:52:48 +1000

    Updated to add modes

--- Functions/shannSeq.m
+++ Functions/shannSeq.m
@@ -1,9 +1,14 @@
-function [shannTab,shannAll] = shannSeq(exLabels)
+function [shannTab,shannAll] = shannSeq(exLabels,strMode)
 %shannSeq - Convert sequential dependencies labels to Shannon entropy values
-%   Takes 'exLabels' as an input (cell array of strings)
-%   If argument empty will calculate Shannon from first principles (based on stored labels)
+%   Takes 'exLabels' as an input (cell array of strings), as well as 'strMode', which is a string of either 'literal' or 'implicit'
+%   This string defines whether to calculate Shannon from literal labels (e.g. "RRRR") or whether to back-convert to sides (e.g. "LLLL")
+%   If exLabels argument empty will calculate Shannon from first principles (based on stored labels)
 
-    disp(['Calculating Shannon entropy of sequences'])
+    if isempty(strMode) == 1
+        strMode = 'literal'; %Default, I suppose
+    end
+    %disp(['Calculating Shannon entropy of sequences'])
+    disp(['Calculating Shannon entropy of sequences with mode ',strMode])
     
     if isempty(exLabels) == 1
         disp(['Performing pre-label processes'])
@@ -20,6 +25,36 @@ function [shannTab,shannAll] = shannSeq(exLabels)
     shannAll = []; %Just the Shannon values, for certain operations
     for s = 1:size(exLabels,2)
         temp = exLabels{s};
+        if strMode == 'implied'
+            temp2 = 0;
+            for i = 1:size(temp,2)
+                if temp(i) == 'R'
+                    temp2(i+1) = temp2(i);
+                else
+                    temp2(i+1) = ~temp2(i);
+                end
+            end
+            temp = temp2;
+        end
+        
+        %New
+        unTemp = unique(temp)';
+        unShann = [];
+        for un = 1:size(unTemp,1)
+            thisProb = nansum( temp == unTemp(un) ) / size(temp,2);
+            unShann(un,1) = -( thisProb )*log2(thisProb);
+        end
+        thisShann = nansum(unShann);
+        shannAll(s) = thisShann;
+        %switch strMode
+            %case 'literal'
+        shannTab{1,s} = temp;
+        shannTab{2,s} = thisShann;
+        shannTab{3,s} = strMode;
+        %end
+
+        %Old
+        %{
         inds{1} = find( temp == 'R' );
         inds{2} = find( temp == 'A' );
         shannTab{1,s} = nan( 1 , size(temp,2) );
@@ -28,6 +63,7 @@ function [shannTab,shannAll] = shannSeq(exLabels)
         ent = abs( wentropy(shannTab{1,s},'shannon') ); %Abs to make plots a bit easier
         shannTab{2,s} = ent;
         shannAll(s) = ent;
+        %}
         disp([num2str(s),' - ',num2str(shannTab{1,s}),' - ',num2str(shannTab{2,s})])
     end
     shannAll = round(shannAll,5); %Round, to make plots a little easier
